fec78e400c8ee714bb2eda0a2ffa4f79461fd5c1
Timothy <timothysimons75@gmail.com>
2019-09-02 11:45:16 +0200

    Create flood method

--- src/simulation/environment.py
+++ src/simulation/environment.py
@@ -3,7 +3,7 @@ import numpy as np
 class Environment:
     """Represent the simulation landscape (the Nile River)."""
 
-    def __init__(self, river_map, fertility_map, shape):
+    def __init__(self, river_map, fertility_map, shape, r_config):
         """Initialise environment attributes upon object instantiation.
 
         Keyword arguments:
@@ -12,9 +12,14 @@ class Environment:
                                (river and arrid land have a pixel value of 0.0 )
         shape               -- shape of all map attributes (tuple)
         """
+        self.FLOOD_FREQ = r_config['flood_frequency']
         self.river_map = river_map
         self.fertility_map = fertility_map
         self.flood_map = np.copy(fertility_map)
-        self.territory_map = np.zeros(shape) # TODO: currently unused
-        self.claim_map = np.zeros(shape)
         self.shape = shape
+
+    def flood(self):
+        flood_freq = self.FLOOD_FREQ
+        if self.FLOOD_FREQ and self.generation % self.FLOOD_FREQ == 0:
+            self.fertility_map = self.flood_map
+            self.flood_map = np.copy(self.fertility_map)
