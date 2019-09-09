b387e2ff8499c7c59df32769a52d08e3fe79b180
Emil <emil.kenguerli@gmail.com>
2019-09-09 12:02:27 +0200

    create testing module

---
+++ src/testing/simulation_test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase, main
+
+class SimulationTest(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    main()
